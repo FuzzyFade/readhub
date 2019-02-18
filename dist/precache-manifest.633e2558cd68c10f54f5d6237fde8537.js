470bca3dd2b5586a48fa860bc0d19b180434dd20
FuzzyFade <1339184537@qq.com>
2019-02-18 17:16:04 +0800

    build file

---
+++ dist/precache-manifest.633e2558cd68c10f54f5d6237fde8537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43464e9c102fc363cd2a",
+    "url": "/js/chunk-vendors.11ec1ad5.js"
+  },
+  {
+    "revision": "841e069b31890d8eaae1",
+    "url": "/js/app.e93f10d1.js"
+  },
+  {
+    "revision": "eeb4ffe37e41cd947ca5cec1edd86a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43464e9c102fc363cd2a",
+    "url": "/css/chunk-vendors.f6d2ddd2.css"
+  },
+  {
+    "revision": "841e069b31890d8eaae1",
+    "url": "/css/app.3879d009.css"
+  }
+];
